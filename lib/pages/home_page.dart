311030100d4610821321885a5ebab57998e3b8e6
refifirmansyah <129053019+refifirmansyah@users.noreply.github.com>
2023-06-02 17:26:51 +0800

    Update home_page.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -20,48 +20,54 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("simple Note App"),
+      appBar: AppBar(
+        title: const Text("simple Note App"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          GoRouter.of(context).goNamed(
+            AppRoutes.addNote,
+          );
+        },
+        child: const Icon(
+          Icons.note_add_rounded,
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            GoRouter.of(context).goNamed(
-              AppRoutes.addNote,
+      ),
+      body: ValueListenableBuilder(
+        valueListenable: Hive.box(DatabaseService.boxName).listenable(),
+        builder: (context, box, child) {
+          if (box.isEmpty) {
+            return Center(
+              child: Text('Tidak ada catatan'),
             );
-          },
-          child: const Icon(
-            Icons.note_add_rounded,
-          ),
-        ),
-        body: ValueListenableBuilder(
-          valueListenable: Hive.box(DatabaseService.boxName).listenable(),
-          builder: (context, box, child) {
-            if (box.isEmpty) {
-              return const Center(
-                child: Text("Tidak ada Catatan"),
-              );
-            } else {
-              return ListView.separated(
-                itemBuilder: (context, index) {
-                  return Dismissible(
-                    key: Key(box.getAt(index).key.toString()),
-                    child: NoteCard(note: box.getAt(index)),
-                    onDismissed: (_) async {
-                      await dbService.deleteNote(box.getAt(index)).then((_) {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(content: Text(box.getAt(index).title)));
-                      });
-                    },
-                  );
-                },
-                separatorBuilder: (context, index) => const SizedBox(
-                  height: 8,
-                ),
-                itemCount: box.length,
-              );
-            }
-          },
-        ));
+          } else {
+            return ListView.separated(
+              itemBuilder: (context, index) {
+                return Dismissible(
+                  key: Key(box.getAt(index).key.toString()),
+                  child: NoteCard(
+                    note: box.getAt(index),
+                  ),
+                  onDismissed: (_) async {
+                    await dbService.deleteNote(box.getAt(index)).then((_) {
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                        content:
+                            Text("${box.getAt(index).title} telah dihapus"
+                            ),
+                      ));
+                    });
+                  },
+                );
+              },
+              separatorBuilder: (context, index) => const SizedBox(
+                height: 8,
+              ),
+              itemCount: box.length,
+            );
+          }
+        },
+      ),
+    );
   }
 }
 
@@ -73,32 +79,32 @@ class NoteCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        margin: EdgeInsets.symmetric(
-          horizontal: 24,
-          vertical: 4,
+      margin: EdgeInsets.symmetric(
+        horizontal: 24,
+        vertical: 4,
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(14),
+        color: Colors.white,
+      ),
+      child: ListTile(
+        onTap: () {
+          GoRouter.of(context).pushNamed(AppRoutes.editNote,
+          extra: note,
+          );
+        },
+        title: Text(
+          note.title,
+          maxLines: 1,
+          overflow: TextOverflow.ellipsis,
+          style: const TextStyle(fontWeight: FontWeight.bold),
         ),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(14),
-          color: Colors.white,
-        ),
-        child: ListTile(
-          onTap: () {
-            GoRouter.of(context).pushNamed(
-              AppRoutes.editNote,
-              extra: note,
-            );
-          },
-          title: Text(
-            note.title,
-            maxLines: 1,
-            overflow: TextOverflow.ellipsis,
-            style: const TextStyle(fontWeight: FontWeight.bold),
+        subtitle: Text(
+          note.description,
+          maxLines: 1,
           ),
-          subtitle: Text(
-            note.description,
-            maxLines: 1,
-          ),
-          trailing: Text(note.createdAt.foramtDate()),
-        ));
+        trailing: Text(note.createdAt.formatDate()),
+      ),
+    );
   }
 }
